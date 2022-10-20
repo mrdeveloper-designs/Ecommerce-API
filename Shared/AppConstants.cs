0c1bc1a156792f5d06b45baf758740f8dea7b2cb
Prakhyath Joshi <prakhyath415@gmail.com>
2022-10-20 22:28:54 +0530

    Working API Code

---
+++ Shared/AppConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace EcommerceAPI.Shared
+{
+    public class AppConstants
+    {
+        public const string AllowedClientDomain = "allowedClient:Domain";
+        public const string CorsPolicyName = "CorsPolicy";
+        public static string DataFder = "DataDirectory";
+        public static string AppData = "App_Data";
+    }
+}
